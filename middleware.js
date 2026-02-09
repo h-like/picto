e61970acc9bd410d91aeb74bcf43890418e11e8c
hyoeunlike@naver.com <hyoeunlike@naver.com>
2026-02-09 15:04:13 +0900

    middleware local add

--- middleware.js
+++ middleware.js
@@ -1,33 +1,41 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
-import { NextResponse } from 'next/server';
-import createMiddleware from 'next-intl/middleware';
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+import createMiddleware from "next-intl/middleware";
 
 const intlMiddleware = createMiddleware({
-  locales: ['en', 'ko'], // 지원할 언어
-  defaultLocale: 'ko'     // 기본 언어
+  locales: ["en", "ko"], // 지원할 언어
+  defaultLocale: "ko", // 기본 언어
 });
 
-const isProtectedRoutes = createRouteMatcher(["/dashboard(.*)", "/editor(.*)"])
+const isProtectedRoutes = createRouteMatcher([
+  "/dashboard(.*)",
+  "/editor(.*)",
+  "/:locale/dashboard(.*)",
+  "/:locale/editor(.*)",
+]);
 
 export default clerkMiddleware(async (auth, req) => {
-  const { userId } = await auth()
+  const { userId } = await auth();
 
-  if(req.nextUrl.pathname.startsWith('/api') || req.nextUrl.pathname.startsWith('/trpc')){
+  if (
+    req.nextUrl.pathname.startsWith("/api") ||
+    req.nextUrl.pathname.startsWith("/trpc")
+  ) {
     return NextResponse.next();
   }
 
-  if(!userId && isProtectedRoutes(req)){
+  if (!userId && isProtectedRoutes(req)) {
     return (await auth()).redirectToSignIn();
   }
 
   return intlMiddleware(req);
-})
+});
 
 export const config = {
   matcher: [
     // 정적 파일 제외
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // API 경로도 반드시 Matcher에 "포함"되어야 Clerk이 작동
-    '/(api|trpc)(.*)',
+    "/(api|trpc)(.*)",
   ],
-}
+};
